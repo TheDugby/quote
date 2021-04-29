ea1175df43b06eca5e7958640b231b77a16e4866
Максим <83405623+TheDugby@users.noreply.github.com>
2021-04-29 18:34:52 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	cout << "\t\t\\ Цитата из фильма \"Джентльмены удачи\" \\ \n";
+	cout << "— Мы вам приклеиваем парик.\n";
+	cout << "— Так.\n";
+	cout << "— Рисуем татуировку.\n";
+	cout << "— Так.\n";
+	cout << "— И сажаем в тюрьму.\n";
+	cout << "— Так… А за что это?\n";
+
+	return 0;
+}
